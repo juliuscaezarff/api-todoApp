df364ce6ed4177e91737e58c7c5051005c2da125
Julius Caezar <julius.caezar25@gmail.com>
2023-07-15 12:21:46 -0400

    refactor: corrigindo o erro de escrita do database

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ import { TodoModule } from './app/todo/todo.module';
         port: Number(configService.get('DB_PORT', 3306)),
         username: configService.get('DB_USERNAME', 'root'),
         password: configService.get('DB_PASSWORD', '123'),
-        database: configService.get('DB_DATAB', 'todo'),
+        database: configService.get('DB_DATABASE', 'todo'),
         entities: [__dirname + '/**/*.entity{.js, .ts}'],
         synchronize: true,
       }),
