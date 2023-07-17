e7eb1240575f68e8732258ac1f0ba9107ce36d0b
Julius Caezar <julius.caezar25@gmail.com>
2023-07-17 16:50:59 -0400

    feat: Criando os controllers com métodos http

--- src/app/todo/todo.controller.ts
+++ src/app/todo/todo.controller.ts
@@ -1,34 +1,50 @@
 /* eslint-disable prettier/prettier */
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseUUIDPipe, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { TodoService } from './todo.service';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
 
 @Controller('api/v1/todos')
 export class TodoController {
   constructor(private readonly todoService: TodoService) {}
-  
+
   @Get()
   async index() {
     return await this.todoService.findAll();
   }
 
   @Post()
-  async create(@Body() body) {
+  async create(@Body() body: CreateTodoDto) {
     return await this.todoService.create(body);
   }
 
   @Get(':id')
-  async show(@Param('id', new ParseUUIDPipe()) id:string) {
+  async show(@Param('id', new ParseUUIDPipe()) id: string) {
     return await this.todoService.findOneOrFail(id);
   }
 
   @Put(':id')
-  async update(@Param('id', new ParseUUIDPipe()) id:string, @Body() body) {
+  async update(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() body: UpdateTodoDto,
+  ) {
     return await this.todoService.update(id, body);
   }
 
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
-  async destroy(@Param('id', new ParseUUIDPipe()) id:string ) {
-    await this.todoService.deleteById(id)
+  async destroy(@Param('id', new ParseUUIDPipe()) id: string) {
+    await this.todoService.deleteById(id);
   }
 }
