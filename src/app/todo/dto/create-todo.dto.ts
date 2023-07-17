d9faaf4de42de78aabab930f15944fe5d5a26831
Julius Caezar <julius.caezar25@gmail.com>
2023-07-17 17:09:09 -0400

    criando dto para a criação de todos

---
+++ src/app/todo/dto/create-todo.dto.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import { IsIn, IsNotEmpty } from 'class-validator';
+
+export class CreateTodoDto {
+  @IsNotEmpty()
+  task: string;
+  
+  @IsNotEmpty()
+  @IsIn([0, 1])
+  isDone: number;
+}
